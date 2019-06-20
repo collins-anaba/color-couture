050e7089182c056389817276f426065e2e2dd432
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-06-20 15:42:40 -0500

    13th commit

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -45,5 +45,36 @@ module.exports = {
     delete: ( req, res) => {
         const dbInstance = req.app.get('db');
         console.log(req.params)
+        dbInstance.deleteProduct(req.params.name)
+        .then((response)=> res.status(200).json(response))
+        .catch(error => {
+            res.status(500).send({errorMessage: 'Error 4'})
+            console.log(error)
+        })
+    },
+    addCart: (req, res) => {
+        const dbInstance = req.app.get('db');
+
+        dbInstance.addToCart(req.params.id)
+        .then((response) => {
+            console.log(response)
+            req.session.user.cart.push({
+            image: response[0].name,
+            style: response[0].style,
+            price: response[0].price,
+            size: response[0].size,
+            description: response[0].description,
+            image: response[0].image
+            })
+            req.session.user.total += response[0].price
+            res.status(200).json(req.session.user)
+        })
+        .catch(error => {
+            res.status(500).send({ errorMessage: "error 5"});
+            console.log(error)
+        })
+    },
+    getCart: (req,res) => {
+        res.status(200).json(req.session.user)
     }
 }

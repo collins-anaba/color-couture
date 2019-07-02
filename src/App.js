87326418f49e5b80ff018dc85f282d8660869423
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-07-02 12:29:21 -0500

    29th commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import routes from './routes';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Link } from 'react-router-dom';
 import store from '../src/redux/store';
 import './App.scss';
 import './reset.css'
 import NavBar from './components/NavBar/NavBar';
+import logo from './images/logo.png';
 // import Home from './components/Home/Home';
 
 
@@ -17,6 +19,7 @@ class App extends Component  {
     <HashRouter>
       <Provider store={store}>
       <div className='App'>
+      <Link to="/"><img src={logo} alt='logo'/></Link>
         <NavBar/>
         {/* <Home/> */}
       {routes}
--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -39,22 +39,25 @@ export default class ContactForm extends Component {
     render() {
         return (
             <div className="contact-page">
-                <h1>Contact Us</h1>
+             <style> @import url('https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap');</style>
+               <h1 className='h1'>Contact Us</h1>
                 {this.state.messageSent === false &&
-                    <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <form className="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
 
-                        <div>
+                        <style> @import url('https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap');</style>
+                        <div className='main-contact'>
                             <label>Name</label>< br />
                             <input type="text" id="name" onChange={this.handleName} />< br />< br />
                             <label>Email address</label>< br />
                             <input type="email" id="email" onChange={this.handleEmail} />< br />< br />
                             <label>Message</label>< br />
                             <textarea rows="5" id="message" onChange={this.handleMessage}></textarea>
+                            <button className='button' type="submit">Submit</button>
                         </div>
-                        <button type="submit">Submit</button>
+                       
                     </form>}
                 {/* next line hides the form */}
-                {this.state.messageSent === true && <h3>Thank you for contacting us. </h3>}
+                {this.state.messageSent === true && <h3 className='h3'>Thank you for contacting us. </h3>}
 
 
             </div>
